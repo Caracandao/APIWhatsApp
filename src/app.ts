63b5e7dd9f0647703a0886c8890c293e0533ea3f
Caracandao <113030508+Caracandao@users.noreply.github.com>
2024-08-25 03:03:14 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ const main = async () => {
 
     await createBot({
         database: new MemoryDB(),
-        flow: createFlow([]),
+        flow: createFlow([flowBienvenida]),
         provider
     })
 }
